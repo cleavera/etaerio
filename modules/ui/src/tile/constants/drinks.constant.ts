447cb4224654274ee763a9719ddb99c93b13f39f
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2018-12-29 14:51:50 +0000

    Update drinks

--- modules/ui/src/tile/constants/drinks.constant.ts
+++ modules/ui/src/tile/constants/drinks.constant.ts
@@ -1,8 +1,8 @@
 export enum Drink {
-    SOFT = 1,
-    WKD = 2,
-    SHORT = 3,
-    BEER = 4,
+    WKD = 1,
+    SHORT = 2,
+    BEER = 3,
+    ALE = 4,
     CIDER = 5,
     WINE = 8,
     SHOT = 10
